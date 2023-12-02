ecb01ad58faac26a0ea645ca2480ed4a048d7251
fayazs <shaikfayaz224@gmail.com>
2023-12-02 22:57:34 +0530

    add new project

---
+++ src/main/java/dao/EmployeeDAO.java
@@ -0,0 +1,21 @@
+package dao;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Primary;
+import com.fayaz.cruddemo.entity.Employee;
+
+@Primary
+public interface EmployeeDAO {
+
+    public void save(Employee theEmployee);//Create
+
+    public List<Employee> findAll();//Read
+
+    public Employee findById(int theId);
+
+    //used saveOrUpdate, so it does both operations No need to write separetly.
+
+    public void deleteById(int theId);//Delete
+
+}
